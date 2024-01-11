8dca8c89d253c46219a2cfe5053a9957f22490e6
Arthur Brigueli <arthurbriguelifort@gmail.com>
2024-01-11 17:02:26 -0300

    feat: adicionado status bar

--- TimePump/App.js
+++ TimePump/App.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import TimeScreen from './screens/TimeScreen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor='#1e1e1e' translucent={false}/>
       <TimeScreen/>
     </View>
   );
