52eb88a900a7c41abb83eb050fd1ceac5565cef8
Arthur Brigueli <arthurbriguelifort@gmail.com>
2024-01-11 15:37:43 -0300

    feat: criação do Timer

--- TimePump/screens/TimeScreen.jsx
+++ TimePump/screens/TimeScreen.jsx
@@ -1,68 +1,119 @@
-import React, {useState, useEffect} from "react";
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
+const TimeScreen = () => {
+  const [tempoTotal, setTempoTotal] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [isDescanso, setIsDescanso] = useState(false)
 
-const TimeScreen = ()=>{
-    const [tempoTotal, setTempoTotal] = useState(0)
-    const [running, setRunning] = useState(false)
+  useEffect(() => {
+    let interval;
 
-    useEffect(()=>{
-        let interval
-
-        if(running){
-            interval = setInterval(()=>{
-                setTempoTotal((prevTempoTotal)=> prevTempoTotal + 1)
-            }, 100)
-        }
-
-        return ()=> clearInterval(interval)
-    }, [running])
-
-    const comecarSerie = ()=>{
-        setRunning(false)
-        setTempoTotal(0)
+    if (running) {
+      interval = setInterval(() => {
+        setTempoTotal((prevTempoTotal) => prevTempoTotal + 1);
+      }, 50);
     }
+    return () => clearInterval(interval);
+  }, [running]);
 
-    const descanso = ()=>{
+  useEffect(()=>{
+    const minuto = Math.floor(tempoTotal / 60);
+    const segundos = tempoTotal % 60;
+
+    if (minuto === 1 && segundos === 0) {
+      handleCallNotifications();
+      setRunning(false);
+    }
+  }, [tempoTotal])
+
+  const comecarSerie = () => {
+    setIsDescanso(false)
+    setRunning(false)
+    setTempoTotal(0);
+  };
+
+  const descanso = () => {
+    if(!isDescanso){
         setRunning(true)
+        setIsDescanso(true)
+    }else{
+        return
+    }
+  };
+
+  const obterMinutos = () => {
+    const minutos = Math.floor(tempoTotal / 60);
+    return minutos < 10 ? `0${minutos}` : minutos;
+  };
+
+  const obterSegundos = () => {
+    const segundos = tempoTotal % 60;
+    return segundos < 10 ? `0${segundos}` : segundos;
+  };
+
+  const handleCallNotifications = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+
+    if (status !== 'granted') {
+      console.log('Você não permitiu a notificação');
+      return;
     }
 
-    const obterMinutos = ()=> {
-        const minutos = Math.floor(tempoTotal/60)
-        return minutos < 10 ? `0${minutos}` : minutos
-    }
-    const segundos = ()=>{
-        const segundos = tempoTotal % 60
-        return segundos < 10 ? `0${segundos}` : segundos
-    }
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "TimePump Alert",
+        body: "Seu tempo de Descanso acabou!"
+      },
+      trigger: {
+        seconds: 1
+      }
+    });
+  };
 
+  return (
+    <View style={styles.container}>
+      {/* Cronômetro */}
+      <View style={styles.cronometroContainer}>
+        {isDescanso ? (
+            <Text style={styles.cronometroText}>{obterMinutos()}:{obterSegundos()}s</Text>
+        ):(
+            <Text style={styles.TextSubTitleCronometro}>Serie em andamento...</Text>
+        )}
 
-    return(
-        <View style={styles.container}>
-            {/* Cronometro */}
-            <View style={styles.cronometroContainer}>
-                <Text style={styles.cronometroText}>{obterMinutos()}:{segundos()}s</Text>
-            </View>
+        {!running && isDescanso && (
+            <Text style={styles.TextSubTitleSerieOff}>Seu tempo de descanso acabou, Inicie a serie!</Text>
+        )}
+        
+    
+      </View>
 
-            {/* botao */}
-            <View style={styles.botoesContainer}>
-                <TouchableOpacity style={styles.botaoComecarSerie} onPress={comecarSerie}>
-                    <Text style={styles.textBotao}>Começar serie</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.botaoDescanso} onPress={descanso}>
-                    <Text style={styles.textBotao}>Descanso</Text>
-                </TouchableOpacity>
-            </View>
-
-
-        </View>
-    )
-}
+      {/* Botões */}
+      <View style={styles.botoesContainer}>
+        <TouchableOpacity style={styles.botaoComecarSerie} onPress={comecarSerie}>
+          <Text style={styles.textBotao}>Começar série</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botaoDescanso} onPress={descanso}>
+          <Text style={styles.textBotao}>Descanso</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 
 const styles = StyleSheet.create({
     container: {
+        flexDirection: 'column',
         justifyContent: 'center',
         alignContent: 'center',
         gap: 100,
@@ -79,7 +130,9 @@ const styles = StyleSheet.create({
     },
 
     botoesContainer: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignContent: 'center'
     },
     botaoComecarSerie: {
         margin: 5,
@@ -98,6 +151,14 @@ const styles = StyleSheet.create({
 
     textBotao: {
         color: 'white'
+    },
+    TextSubTitleCronometro: {
+        color: 'gray',
+        fontSize: 25
+    },
+
+    TextSubTitleSerieOff: {
+        color: 'gray',
     }
 
 })
