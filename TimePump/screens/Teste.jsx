beb27fecbe37a08f08b5c70b280237a41bcd2546
Arthur Brigueli <arthurbriguelifort@gmail.com>
2024-01-12 03:50:06 -0300

    Create: criação das screens

---
+++ TimePump/screens/Teste.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Teste = ()=>{
+    return(
+        <View>
+            <Text>skaskaskaks</Text>
+        </View>
+    )
+}
+
+export default Teste
